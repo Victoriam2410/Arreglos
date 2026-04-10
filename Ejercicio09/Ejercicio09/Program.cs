8cdac526e4e307b9bcbafb100ec538f2cb5e9cce
Victoriam2410 <estherrodas61@gmail.com>
2026-04-09 20:09:06 -0600

    Add files via upload

---
+++ Ejercicio09/Ejercicio09/Program.cs
@@ -0,0 +1,29 @@
+﻿//Solicitar n números, guardarlos en un arreglo y buscar un valor indicado por el usuario
+int ingreso;
+Console.WriteLine("cuantas números desea ingresar?: ");
+ingreso = int.Parse(Console.ReadLine());
+int[] numero = new int[ingreso];
+
+for (int i = 0; i < numero.Length; i++)
+{
+    Console.WriteLine("ingrese numero: ");
+    numero[i] = int.Parse(Console.ReadLine());
+}
+
+Console.WriteLine("ingrese el valor que desea buscar: ");
+int buscar = int.Parse(Console.ReadLine());
+int coincidencias = 0;
+
+for (int i = 0; i < numero.Length; i++)
+{
+    if (numero[i] == buscar)
+    {
+        Console.WriteLine($"valor encontrado en la posicion: {i}");
+        coincidencias++;
+    }
+}
+
+if (coincidencias == 0)
+{
+    Console.WriteLine("el valor no se encuentra en el arreglo");
+}
