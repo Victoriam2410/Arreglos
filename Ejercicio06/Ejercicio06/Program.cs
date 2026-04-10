089ba5289e319f148bd209d8050aaa66bd7d5463
Victoriam2410 <estherrodas61@gmail.com>
2026-04-09 20:07:47 -0600

    Add files via upload

---
+++ Ejercicio06/Ejercicio06/Program.cs
@@ -0,0 +1,16 @@
+﻿//Solicitar n números en un arreglo y determinar el valor menor
+int ingreso;
+Console.WriteLine("cuantas números desea ingresar?: ");
+ingreso = int.Parse(Console.ReadLine());
+int[] numero = new int[ingreso];
+int menor = numero[0];
+for (int i = 0; i < numero.Length; i++)
+{
+    Console.WriteLine("ingrese numero: ");
+    numero[i] = int.Parse(Console.ReadLine());
+    if (i == 0 || numero[i] < menor)
+    {
+        menor = numero[i];
+    }
+}
+Console.WriteLine($"menor: {menor}");
