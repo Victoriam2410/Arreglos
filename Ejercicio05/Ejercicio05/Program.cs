e077c33506a9f4075a0d56e5e560baaec8bae902
Victoriam2410 <estherrodas61@gmail.com>
2026-04-09 19:42:59 -0600

    Add files via upload

---
+++ Ejercicio05/Ejercicio05/Program.cs
@@ -0,0 +1,16 @@
+﻿//Solicitar n números en un arreglo y determinar el valor mayor
+int ingreso;
+Console.WriteLine("cuantas números desea ingresar?: ");
+ingreso = int.Parse(Console.ReadLine());
+int[] numero = new int[ingreso];
+int mayor = numero[0];
+for (int i = 0; i < numero.Length; i++)
+{
+    Console.WriteLine("ingrese numero: ");
+    numero[i] = int.Parse(Console.ReadLine());
+    if (numero[i] > mayor)
+    {
+        mayor = numero[i];
+    }
+}
+Console.WriteLine($"Mayor: {mayor}");
