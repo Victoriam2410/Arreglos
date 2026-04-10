9cefb4bb2c038248387289e3b1268cde6dc5c7fe
Victoriam2410 <estherrodas61@gmail.com>
2026-04-09 19:17:39 -0600

    Add files via upload

---
+++ ejercicio01/ejercicio01/Program.cs
@@ -0,0 +1,15 @@
+﻿//Solicitar al usuario la cantidad n de números, guardarlos en un arreglo y mostrarlos en pantalla
+int ingreso;
+Console.WriteLine("cuantas números desea ingresar?: ");
+ingreso = int.Parse(Console.ReadLine());
+int[] numero = new int[ingreso];
+for (int i = 0; i < numero.Length; i++)
+{
+    Console.WriteLine("ingrese numero: ");
+    numero[i] = int.Parse(Console.ReadLine());
+}
+Console.WriteLine("Los numeros ingresados son:");
+for (int i = 0; i < numero.Length; i++)
+{
+    Console.WriteLine(numero[i]);
+}
