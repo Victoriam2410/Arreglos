3e19d56838c549984f6baac777bfc8d408067d62
Victoriam2410 <estherrodas61@gmail.com>
2026-04-09 20:07:23 -0600

    Add files via upload

---
+++ Ejercicio04/Ejercicio04/Program.cs
@@ -0,0 +1,14 @@
+﻿//Solicitar n números, guardarlos en un arreglo y calcular el promedio
+int ingreso;
+int suma = 0;
+Console.WriteLine("cuantas números desea ingresar?: ");
+ingreso = int.Parse(Console.ReadLine());
+int[] numero = new int[ingreso];
+for (int i = 0; i < numero.Length; i++)
+{
+    Console.WriteLine("ingrese edad: ");
+    numero[i] = int.Parse(Console.ReadLine());
+    suma += numero[i];
+}
+Console.WriteLine($"el promedio de los numeros es: {suma / ingreso}");
+
