2492b07efc44e21f3bc0233b29263f3035be6deb
Victoriam2410 <estherrodas61@gmail.com>
2026-04-09 20:10:16 -0600

    Add files via upload

---
+++ Ejercicio12/Ejercicio12/Program.cs
@@ -0,0 +1,20 @@
+﻿//Solicitar n números y mostrar únicamente los valores positivos
+int ingreso;
+Console.WriteLine("cuantas números desea ingresar?: ");
+ingreso = int.Parse(Console.ReadLine());
+int[] numero = new int[ingreso];
+
+for (int i = 0; i < numero.Length; i++)
+{
+    Console.WriteLine("ingrese numero: ");
+    numero[i] = int.Parse(Console.ReadLine());
+}
+
+Console.WriteLine("valores positivos:");
+for (int i = 0; i < numero.Length; i++)
+{
+    if (numero[i] > 0)
+    {
+        Console.WriteLine(numero[i]);
+    }
+}
