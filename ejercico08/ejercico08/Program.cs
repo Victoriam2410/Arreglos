5981d9b0452126c30d0fbd06995d6a0a0dcf5a30
Victoriam2410 <estherrodas61@gmail.com>
2026-04-09 20:08:44 -0600

    Add files via upload

---
+++ ejercico08/ejercico08/Program.cs
@@ -0,0 +1,20 @@
+﻿//Llenar un arreglo con n números y contar cuántos son impares
+int ingreso;
+Console.WriteLine("cuantas números desea ingresar?: ");
+ingreso = int.Parse(Console.ReadLine());
+int[] numero = new int[ingreso];
+int impares = 0;
+
+for (int i = 0; i < numero.Length; i++)
+{
+    Console.WriteLine("ingrese numero: ");
+    numero[i] = int.Parse(Console.ReadLine());
+
+    if (numero[i] % 2 != 0)
+    {
+        impares++;
+    }
+}
+
+Console.WriteLine($"cantidad de impares: {impares}");
+
