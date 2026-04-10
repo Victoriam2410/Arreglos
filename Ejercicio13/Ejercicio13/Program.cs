63fd8bf3db7344ac50e4d7f6449969b32bcd93a8
Victoriam2410 <estherrodas61@gmail.com>
2026-04-09 20:10:36 -0600

    Add files via upload

---
+++ Ejercicio13/Ejercicio13/Program.cs
@@ -0,0 +1,24 @@
+﻿//Crear un arreglo de tamaño n, llenarlo y copiar sus valores a otro arreglo
+int ingreso;
+Console.WriteLine("cuantas números desea ingresar?: ");
+ingreso = int.Parse(Console.ReadLine());
+
+int[] numero = new int[ingreso];
+int[] copia = new int[ingreso];
+
+for (int i = 0; i < numero.Length; i++)
+{
+    Console.WriteLine("ingrese numero: ");
+    numero[i] = int.Parse(Console.ReadLine());
+}
+
+for (int i = 0; i < numero.Length; i++)
+{
+    copia[i] = numero[i];
+}
+
+Console.WriteLine("valores en el arreglo copia:");
+for (int i = 0; i < copia.Length; i++)
+{
+    Console.WriteLine(copia[i]);
+}
