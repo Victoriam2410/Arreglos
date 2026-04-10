f4c67ec17b511d05f54f12d9d6bbfa1514817b6f
Victoriam2410 <estherrodas61@gmail.com>
2026-04-09 20:05:10 -0600

    Add files via upload

---
+++ Ejercicio02/Ejercicio02/Program.cs
@@ -0,0 +1,15 @@
+﻿//Solicitar n números, guardarlos en un arreglo y mostrarlos en orden inverso
+int ingreso;
+Console.WriteLine("cuantas números desea ingresar?: ");
+ingreso = int.Parse(Console.ReadLine());
+int[] numero = new int[ingreso];
+for (int i = 0; i < numero.Length; i++)
+{
+    Console.WriteLine("ingrese numero: ");
+    numero[i] = int.Parse(Console.ReadLine());
+}
+Console.WriteLine("Los numeros ingresados son:");
+for (int i = numero.Length - 1; i >= 0; i--)
+{
+    Console.WriteLine(numero[i]);
+}
