9992b52fba6ffa464f824d4ac66aa23ff0a37f7a
Victoriam2410 <estherrodas61@gmail.com>
2026-04-09 20:08:20 -0600

    Add files via upload

---
+++ Ejercicio07/Ejercicio07/Program.cs
@@ -0,0 +1,19 @@
+﻿//Llenar un arreglo con n números y contar cuántos son pares
+int ingreso;
+Console.WriteLine("cuantas números desea ingresar?: ");
+ingreso = int.Parse(Console.ReadLine());
+int[] numero = new int[ingreso];
+int pares = 0;
+
+for (int i = 0; i < numero.Length; i++)
+{
+    Console.WriteLine("ingrese numero: ");
+    numero[i] = int.Parse(Console.ReadLine());
+
+    if (numero[i] % 2 == 0)
+    {
+        pares++;
+    }
+}
+
+Console.WriteLine($"cantidad de pares: {pares}");
