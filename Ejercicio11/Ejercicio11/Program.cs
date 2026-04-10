b2fff30e972341ec27c2c6b8a45cc60d9755a763
Victoriam2410 <estherrodas61@gmail.com>
2026-04-09 20:09:51 -0600

    Add files via upload

---
+++ Ejercicio11/Ejercicio11/Program.cs
@@ -0,0 +1,25 @@
+﻿//Llenar un arreglo con n números, solicitar una posición y reemplazar su valor
+int ingreso;
+Console.WriteLine("cuantas números desea ingresar?: ");
+ingreso = int.Parse(Console.ReadLine());
+int[] numero = new int[ingreso];
+
+for (int i = 0; i < numero.Length; i++)
+{
+    Console.WriteLine("ingrese numero: ");
+    numero[i] = int.Parse(Console.ReadLine());
+}
+
+Console.WriteLine("ingrese la posicion que desea modificar: ");
+int posicion = int.Parse(Console.ReadLine());
+
+Console.WriteLine("ingrese el nuevo valor: ");
+int nuevoValor = int.Parse(Console.ReadLine());
+
+numero[posicion] = nuevoValor;
+
+Console.WriteLine("arreglo actualizado:");
+for (int i = 0; i < numero.Length; i++)
+{
+    Console.WriteLine($"posicion {i}: {numero[i]}");
+}
