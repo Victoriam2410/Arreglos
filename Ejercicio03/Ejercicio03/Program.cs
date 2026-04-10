258dbe10695d3caa156ded2dbbb7009145a4797a
Victoriam2410 <estherrodas61@gmail.com>
2026-04-09 20:06:17 -0600

    Add files via upload

---
+++ Ejercicio03/Ejercicio03/Program.cs
@@ -0,0 +1,13 @@
+﻿//Solicitar n números enteros, guardarlos en un arreglo y calcular la suma total
+int ingreso;
+int suma = 0;
+Console.WriteLine("cuantas números desea ingresar?: ");
+ingreso = int.Parse(Console.ReadLine());
+int[] numero = new int[ingreso];
+for (int i = 0; i < numero.Length; i++)
+{
+    Console.WriteLine("ingrese numero: ");
+    numero[i] = int.Parse(Console.ReadLine());
+    suma += numero[i];
+}
+Console.WriteLine($"la suma es: {suma}");
